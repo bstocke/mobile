90de9af0dde55d7a429c1cf3ca392abcea145b87
bstocke <dueling3500@gmail.com>
2022-04-12 15:32:27 -0400

    commit on 'master'

---
+++ MyMovieApp/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, Veiw } from 'react-native';
+
+const MovieApp = () => {
+  return(
+    <Veiw style={{ flex: 1, justifyContent: "Center", alignItems: "center" }}>
+    <Text>
+      Hello World
+    </Text>
+    </Veiw>
+  );
+};
+
+export default MovieApp;
